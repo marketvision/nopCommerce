c0328a53827ff825e9bb5b3934b56efc6b0a233d
mynkow <mynkow@gmail.com>
2019-08-05 09:33:32 +0300

    Adds newgen import submodul

--- local.cmd
+++ local.cmd
@@ -1,2 +1,3 @@
 dotnet build .\src\NopCommerce.sln
 dotnet build .\src\Plugins\Nop.Plugin.ExternalAuth.oAuth2\Nop.Plugin.ExternalAuth.oAuth2.csproj
+dotnet build .\src\Plugins\Nop.Plugin.Misc.NewGen\Nop.Plugin.Misc.NewGen.csproj
